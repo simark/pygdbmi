74b7a2fe93fb77d7f2600e8caa650119fb14b1a1
Simon Marchi <simon.marchi@polymtl.ca>
2015-02-28 13:06:23 -0500

    Cleanup pprint_test.py

--- tests/pprint_test.py
+++ tests/pprint_test.py
@@ -5,11 +5,11 @@ from pygdbmi import visitors
 
 import io
 
+
 class TestPPrintVisitor(unittest.TestCase):
 
     def setUp(self):
         pass
-
 
     def _test_pprint(self, input_, expected_output):
         f = io.StringIO()
@@ -22,8 +22,15 @@ class TestPPrintVisitor(unittest.TestCase):
 
         self.assertEqual(result, expected_output)
 
-    def test_empty(self):
-        input_ = '^done,numchild="2",displayhint="array",children=[child={name="var3.[0]",exp="[0]",numchild="1",type="my_class",thread-id="1"},child={name="var3.[1]",exp="[1]",numchild="1",type="my_class",thread-id="1"}],has_more="0"\n'
+    def test_simple(self):
+        self._test_pprint('^done\n', '^done\n')
+        self._test_pprint('^done,a="2"\n', '^done,\n  a = "2"\n')
+
+    def test_one(self):
+        input_ = '^done,numchild="2",displayhint="array",children=[child={' \
+                 'name="var3.[0]",exp="[0]",numchild="1",type="my_class",' \
+                 'thread-id="1"},child={name="var3.[1]",exp="[1]",numchild=' \
+                 '"1",type="my_class",thread-id="1"}],has_more="0"\n'
         expected_output = \
             '^done,\n' \
             '  numchild = "2",\n' \
@@ -47,6 +54,3 @@ class TestPPrintVisitor(unittest.TestCase):
             '  has_more = "0"\n'
 
         self._test_pprint(input_, expected_output)
-
-        self.assertTrue(True)
-
